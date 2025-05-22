7125503b3300edafa730924bc0b1fb4648ba231b
Adam <sfadam@stanford.edu>
2025-05-22 15:48:43 -0700

    minor

--- install-all-modified.sh
+++ install-all-modified.sh
@@ -2,5 +2,5 @@ sudo cp modified/tcp_output.c ./cs244-linux-5.19.10/net/ipv4/
 sudo cp modified/tcp_input.c ./cs244-linux-5.19.10/net/ipv4/
 sudo cp modified/tcp_cubic.c ./cs244-linux-5.19.10/net/ipv4/
 sudo cp modified/tcp_cong.c ./cs244-linux-5.19.10/net/ipv4/
-sudo cp modifiedtcp.h ./cs244-linux-5.19.10/include/linux
+sudo cp modified/tcp.h ./cs244-linux-5.19.10/include/linux
 
